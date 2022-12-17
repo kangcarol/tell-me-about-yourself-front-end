f7bc70567b8981b5aa8aae1bd28dd642e473da8a
kangcarol <otherhats@gmail.com>
2022-12-17 18:13:16 -0500

    clean up list page

--- src/pages/CardList/CardList.jsx
+++ src/pages/CardList/CardList.jsx
@@ -7,146 +7,77 @@ const CardList = (props) => {
   console.log("cardlist props:" , props)
   return (
     <main className={styles.container}>
-      <section>
+
         <div>
           <Link to="/cards/behavioral">
             <Icon category={'behavioral'} />
             <h1>BEHAVIORAL</h1>
           </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Behavioral') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/computerscience">
           <Icon category={'computerscience'} />
           <h1>COMPUTER SCIENCE</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Computer Science') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
       <div>
       <Link to="/cards/javascript">
         <Icon category={'javascript'} />
         <h1>JAVASCRIPT</h1>
       </Link>
       </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Javascript') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/python">
           <Icon category={'python'} />
           <h1>PYTHON</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Python') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/frontend">
           <Icon category={'frontend'} />
           <h1>FRONT END</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Frontend') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/react">
           <Icon category={'react'} />
           <h1>REACT</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'React') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/styling">
           <Icon category={'styling'} />
           <h1>STYLING</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Styling') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/backend">
           <Icon category={'backend'} />
           <h1>BACK END</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Backend') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/databases">
           <Icon category={'databases'} />
           <h1>DATABASES</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Databases') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
-      <section>
+
         <div>
         <Link to="/cards/other">
           <Icon category={'other'} />
           <h1>OTHER</h1>
         </Link>
         </div>
-        {/* {props.card.map((card, idx) => (
-          (card.category === 'Other') ?
-          <Card card={card} handleAddFavorites={props.handleAddFavorites} key={card._id} />
-          :
-          ''
-        ))} */}
-      </section>
+
     </main>
   )
 }
