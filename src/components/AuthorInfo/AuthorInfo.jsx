4be95735a0735d3e41ea3f4402a3ac85853b54bc
kangcarol <otherhats@gmail.com>
2023-01-18 03:01:13 -0500

    author img css tweaking

--- src/components/AuthorInfo/AuthorInfo.jsx
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -6,7 +6,7 @@ const AuthorInfo = ({ content }) => {
   const photo = content.author.photo ? content.author.photo : ProfileIcon
   return (
     <div className={styles.container}>
-        <img src={photo} alt="The user's avatar" />
+        <img src={photo} alt="The user's avatar" className={styles.container}/>
       <div>
         <h4>contributed by</h4>
         <h4>{content.author.name.toUpperCase()}</h4>
